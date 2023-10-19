af391739fb3705c4ded9c88da0653659cfb1aa7c
Brayan Henao <bhenao@vmware.com>
2023-10-18 22:22:23 -0400

    fix: added config to make it run in eb

---
+++ application.py
@@ -0,0 +1,7 @@
+from src.main import app as application
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
